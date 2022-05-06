737c99f1245b2554575d05f803b03995c9e23216
Daryassatya <daryassatya@gmail.com>
2022-05-06 14:38:08 +0700

    Tahap 1 - Routing Dasar

---
+++ routes/web.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+ */
+
+// First Default Routes
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/', function () {
+    return view('home');
+});
+Route::get('/about', function () {
+    return view('about', [
+        'nama' => "Dimas Aryasatya",
+        'email' => "daryassatya@gmail.com"
+    ]);
+});
+Route::get('/posts', function () {
+    return view('posts');
+});
